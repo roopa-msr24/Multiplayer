6dedf53d304fafdefd226d7e6e8668419c4f11a9
roopa-msr24 <74906011+roopa-msr24@users.noreply.github.com>
2021-11-27 23:06:09 +0530

    Update Form.js

--- js/Form.js
+++ js/Form.js
@@ -8,8 +8,8 @@ class Form {
 
   setElementsPosition() {
     this.titleImg.position(120, 160);
-    this.input.position(width / 2 - 110, height / 2 +30);
-    this.playButton.position(width / 2 - 90, height / 2 +80);
+    this.input.position(width / 2 - 110, height / 2 +80);
+    this.playButton.position(width / 2 - 90, height / 2 +150);
     this.greeting.position(width / 2 - 300, height / 2 - 100);
   }
 
